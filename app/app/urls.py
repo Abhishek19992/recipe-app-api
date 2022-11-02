0ebba42ac9ed5e8a4ab03af6ce326aa9401c3fea
Abhishek Khamkar <askhamkar19@gmail.com>
2022-11-02 23:21:15 +0530

    Added recipe model updated user model. Added new models in models file. Added new test.

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,11 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Documentation Imports
+from xml.etree.ElementInclude import include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 # This is updated urls file
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    ),
+    path('api/user/',include('user.urls')),
+    path('api/recipe/',include('recipe.urls')),
 ]
--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,8 @@
 """
 Database models.
 """
-
+from unicodedata import decimal
+from django.conf import settings
 from email.policy import default
 from enum import unique
 from django.db import models
@@ -46,3 +47,43 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
+
+    def __str__(self):
+        return self.title
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    """Ingredient for recipe."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+
+    def __str__(self):
+        return self.name
+    
